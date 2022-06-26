cd222e5e230416f421b477ff8c63188fa3551e0d
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-06-26 16:56:13 -0700

    fixed upload

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,85 +1,54 @@
-import middleware from '../../middleware/middleware'
-import nextConnect from 'next-connect'
+import middleware from "../../middleware/middleware";
+import nextConnect from "next-connect";
+import { Web3Storage, File, getFilesFromPath } from "web3.storage";
+const { join, resolve } = require("path");
 
-const handler = nextConnect()
-handler.use(middleware)
+const handler = nextConnect();
+handler.use(middleware);
 
 handler.post(async (req, res) => {
-  console.log("BODY", req.body.text)
-  console.log("FILES", req.files.file)
 
-  //...
-})
+  try {
+    const files = await makeFileObjects(req.body, req.files);
+    const cid = await storeFiles(files);
+    console.log("stored files with cid:", cid);
+    return res.status(200).json({ success: true, cid: cid });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ error: "Error storing the file", success: false });
+  }
+});
 
 export const config = {
   api: {
-    bodyParser: false
+    bodyParser: false,
+  },
+};
+
+export default handler;
+
+
+async function storeFiles(files) {
+  const client = makeStorageClient();
+  try {
+    const cid = await client.put(files);
+    return cid;
+  } catch (error) {
+    console.log("ERROR", error);
   }
 }
 
-export default handler
+async function makeFileObjects(text, myFiles) {
+  const buffer = Buffer.from(JSON.stringify(text));
+  let filename = myFiles['file'][0].path
+  const imageDirectory = resolve(process.cwd(), filename);
+  const files = await getFilesFromPath(imageDirectory);
 
+  files.push(new File([buffer], "data.json"));
+  return files;
+}
 
-// import { Web3Storage, File, getFilesFromPath } from "web3.storage";
-// const { join, resolve } = require("path");
-
-
-// export default async function handler(req, res) {
-//   console.log("HANDLING")
-//   if (req.method === "POST") {
-//     console.log("POST REQ", Object.keys(req))
-//     return res.status(200)
-//     // return await storeEventData(req, res);
-//   } else {
-//     return res
-//       .status(405)
-//       .json({ message: "Method not allowed", success: false });
-//   }
-// }
-
-// async function storeEventData(req, res) {
-
-//   const fileBlob = [
-//     new File(['contents-of-file-1'], 'plain-utf8.txt'),
-//     new File([req.body], "myNewFile")
-//   ]
-
-//   // console.log("FILES", fileBlob.stream)
-//   try {
-//     const cid = await storeFiles(fileBlob);
-//     console.log("stored files with cid:", cid);
-//     return res.status(200).json({ success: true, cid: cid });
-//   } catch (err) {
-//     return res
-//       .status(500)
-//       .json({ error: "Error storing the file", success: false });
-//   }
-// }
-
-// async function storeFiles(files) {
-//   console.log("TRYING TO STORE")
-//   const client = makeStorageClient();
-//   console.log("CLIENT PUT")
-//   try {
-//     const cid = await client.put(files);
-//     return cid;
-//   } catch (error) {
-//     console.log("ERROR", error)
-//   }
-// }
-
-// // async function makeFileObjects(body) {
-// //   const buffer = Buffer.from(JSON.stringify(body));
-
-// //   const imageDirectory = resolve(process.cwd(), `public/images/mojito.png`);
-// //   const files = await getFilesFromPath(imageDirectory);
-
-// //   files.push(new File([buffer], "data.json"));
-// //   console.log(files);
-// //   return files;
-// // }
-
-// function makeStorageClient() {
-//   console.log("MAKING CLIENT")
-//   return new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN });
-// }
+function makeStorageClient() {
+  return new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN });
+}
