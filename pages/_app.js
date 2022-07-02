87b79229986d35e155c25c0d68b3621c187738d4
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-07-02 11:46:33 -0700

    dynamic download page

--- pages/_app.js
+++ pages/_app.js
@@ -1,41 +1,41 @@
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { infuraProvider } from "wagmi/providers/infura";
-import { publicProvider } from "wagmi/providers/public";
+// import "@rainbow-me/rainbowkit/styles.css";
+// import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+// import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+// import { infuraProvider } from "wagmi/providers/infura";
+// import { publicProvider } from "wagmi/providers/public";
 // import { ApolloProvider } from "@apollo/client";
 // import client from "../apollo-client";
 // import Layout from "../components/Layout";
 import "../styles/globals.css";
 
-const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
+// const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
 
-const { chains, provider } = configureChains(
-  [chain.polygon],
-  [infuraProvider({ infuraId }), publicProvider()]
-);
+// const { chains, provider } = configureChains(
+//   [chain.polygon],
+//   [infuraProvider({ infuraId }), publicProvider()]
+// );
 
-const { connectors } = getDefaultWallets({
-  appName: "web3rsvp",
-  chains,
-});
+// const { connectors } = getDefaultWallets({
+//   appName: "web3rsvp",
+//   chains,
+// });
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
+// const wagmiClient = createClient({
+//   autoConnect: true,
+//   connectors,
+//   provider,
+// });
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        {/* <ApolloProvider client={client}> */}
-          {/* <Layout> */}
+    // <WagmiConfig client={wagmiClient}>
+    //   <RainbowKitProvider chains={chains}>
+        // {/* <ApolloProvider client={client}> */}
+         //<Layout>
             <Component {...pageProps} />
-          {/* </Layout> */}
-        {/* </ApolloProvider> */}
-      </RainbowKitProvider>
-    </WagmiConfig>
+        //</Layout>
+        // {/* </ApolloProvider> */}
+    //   </RainbowKitProvider>
+    // </WagmiConfig>
   );
 }
--- pages/download/[cid].js
+++ pages/download/[cid].js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { create } from "ipfs-http-client";
-import Lit from "../utils/getLit";
+import { useRouter } from "next/router";
+import Lit from "../../utils/getLit";
 
 const accessControlConditions = [
   {
@@ -15,35 +16,41 @@ const accessControlConditions = [
     },
   },
 ];
-// unencrypted
-// const cid = "bafybeidb3dbhebqfltauasfp2xvaxa5tz6bg2i3xv6h5rb3yabqcmi3yhe";
-// encrypted
-const cid = "bafybeienxmzvpnzvx2676kikekb2vfbssj2euqbms63fl4dp2jqlu4u6oa"
 
 export default function Download() {
-  const [loading, setLoading] = useState(false)
-  const [encrypted, setEncrypted] = useState(true);
+  const router = useRouter();
+  const { cid } = router.query;
+
+  const [loading, setLoading] = useState(false);
+  const [encrypted, setEncrypted] = useState(false);
   const [rawFiles, setRawFiles] = useState(null);
   const [returnedFiles, setReturnedFiles] = useState(null);
   const [downloadFileName, setDownloadFileName] = useState("");
-  const [links, setLinks] = useState(null)
-  const [projectInfo, setProjectInfo] = useState(null)
+  const [links, setLinks] = useState(null);
+  const [projectInfo, setProjectInfo] = useState(null);
 
   useEffect(() => {
     async function runGetLinks() {
-      const myLinks = await getLinks(cid)
-      setLinks(myLinks)
+      const myLinks = await getLinks(cid);
+      setLinks(myLinks);
     }
 
     async function getProjectInfo() {
-      const res = await fetch(`https://ipfs.io/ipfs/${cid}/data.json`)
-      const json = await res.json()
-      setProjectInfo(json)
+      const res = await fetch(`https://ipfs.io/ipfs/${cid}/data.json`);
+      const json = await res.json();
+      if(json.key){
+        setEncrypted(true)
+      }
+      setProjectInfo(json);
+      
     }
 
-    runGetLinks();
-    getProjectInfo();
-  }, [])
+    if(cid){
+      runGetLinks();
+      getProjectInfo();
+    }
+    
+  }, [cid]);
 
   async function getLinks(ipfsPath) {
     const url = "https://dweb.link/api/v0";
@@ -56,35 +63,34 @@ export default function Download() {
   }
 
   const prepareDownload = async () => {
-    setLoading(true)
+    setLoading(true);
     if (!encrypted) {
       let raw = [];
       for await (const link of links) {
-        if(link.name !== "data.json"){
-          let res = await fetch(`https://ipfs.io/ipfs/${link.path}`)
-          let blob = await res.blob()
+        if (link.name !== "data.json") {
+          let res = await fetch(`https://ipfs.io/ipfs/${link.path}`);
+          let blob = await res.blob();
           const file = {
-                path: URL.createObjectURL(blob),
-                name: link.name
-              }
+            path: URL.createObjectURL(blob),
+            name: link.name,
+          };
           raw.push(file);
         }
       }
-      setRawFiles(raw)
-      setLoading(false)
+      setRawFiles(raw);
+      setLoading(false);
     } else {
       let lit = new Lit();
       await lit.connect();
-      console.log("CONNECTED");
-      const res =  await fetch('https://ipfs.io/ipfs/bafybeienxmzvpnzvx2676kikekb2vfbssj2euqbms63fl4dp2jqlu4u6oa/aENQvwJwnRg4NFG0Yv0PvAH4.zip')
-      let blob = await res.blob()
+      const res = await fetch(
+        `https://ipfs.io/ipfs/${cid}/${links[0].name}`
+      );
+      let blob = await res.blob();
       const { decryptedZip } = await lit.decryptFiles(
         accessControlConditions,
         blob,
-        "0fb19fab5b1669ece749596747e8c9611b77275dfb5503c7ca4c4f3e511b526fab3d5682fa791b9b78957c0bac522f0a8afde462fba60831aceb4edfc61b83bac164544e51f8be2061094feaa6db465b8c4cdb1f901418b3098361da3adc29b5b8c0aacd2462cc9e569c3c399221e4fb22175b6aa9a0841e98572fc73554528c0000000000000020c6e3764d11ba5635e5260a852777f75b18ce6963c28ef1174b10b0384c661cfa3d5ea6b3ab89bcdd2abd3a0b3c98158c"
+        projectInfo.key[0]
       );
-      console.log("DECRYPTED ZIP", decryptedZip);
-
       let filePath;
 
       for (let item of Object.values(decryptedZip)) {
@@ -104,7 +110,7 @@ export default function Download() {
       setReturnedFiles(URL.createObjectURL(gltfBlob));
 
       setDownloadFileName(filePath.substring(filePath.lastIndexOf("/") + 1));
-      setLoading(false)
+      setLoading(false);
     }
   };
 
@@ -112,12 +118,16 @@ export default function Download() {
     <div>
       <h1>Download Files</h1>
 
-      {projectInfo?.name && (<div>Project Name: {projectInfo.text[0]}</div>)}
-      {projectInfo?.description && (<div>Description: {projectInfo.description[0]}</div>)}
+      {projectInfo?.name && <div>Project Name: {projectInfo.name[0]}</div>}
+      {projectInfo?.description && (
+        <div>Description: {projectInfo.description[0]}</div>
+      )}
 
       {!returnedFiles && !downloadFileName && !loading && (
         <button onClick={() => prepareDownload()}>Prepare Download</button>
       )}
+
+      <button onClick={() => {console.log(links)}}>Links</button>
 
       {returnedFiles && downloadFileName && !loading && (
         <div>
@@ -132,7 +142,12 @@ export default function Download() {
       {rawFiles && !loading && (
         <div>
           {rawFiles.map((file) => (
-            <a href={file.path} download={file.name} key={file.path} style={{marginBottom: "20px", display: "block"}}>
+            <a
+              href={file.path}
+              download={file.name}
+              key={file.path}
+              style={{ marginBottom: "20px", display: "block" }}
+            >
               Download {file.name}
             </a>
           ))}
