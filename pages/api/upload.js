d5d0df3ee1f816178c2f5e71da2af369cb60283f
sarahschwartz <58856580+sarahschwartz@users.noreply.github.com>
2022-07-04 10:26:54 -0700

    keep original filename

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,22 +1,21 @@
 import middleware from "../../middleware/middleware";
 import nextConnect from "next-connect";
 import { Web3Storage, File, getFilesFromPath } from "web3.storage";
-const fs = require('fs');
-const { resolve } = require("path");
+const fs = require("fs");
+const { resolve, join, dirname } = require("path");
 
 const handler = nextConnect();
 handler.use(middleware);
 
 handler.post(async (req, res) => {
-
   try {
-    console.log("BODY", req.body)
-    console.log("FILES", req.files)
+    console.log("BODY", req.body);
+    console.log("FILES", req.files);
     const files = await makeFileObjects(req.body, req.files);
     const cid = await storeFiles(files);
     console.log("stored files with cid:", cid);
-    return res.status(200)
-    .json({ success: true, cid: cid });
+
+    return res.status(200).json({ success: true, cid: cid });
   } catch (err) {
     return res
       .status(500)
@@ -32,7 +31,6 @@ export const config = {
 
 export default handler;
 
-
 async function storeFiles(files) {
   const client = makeStorageClient();
   try {
@@ -47,17 +45,21 @@ async function makeFileObjects(text, myFiles) {
   let files;
   const buffer = Buffer.from(JSON.stringify(text));
   for (let item of Object.values(myFiles)) {
-    const filepath = resolve(process.cwd(), item[0].path);
-    if(!files){
-      files = await getFilesFromPath(filepath);
+    if (item[0].originalFilename && item[0].originalFilename !== "") {
+      const filePath = resolve(process.cwd(), item[0].path);
+      const newPath = join(dirname(filePath), item[0].originalFilename);
+      await fs.promises.rename(filePath, newPath);
+    }
+    if (!files) {
+      files = await getFilesFromPath(newPath);
     } else {
-      let newFiles = await getFilesFromPath(filepath);
-      files = [...files, ...newFiles]
+      let newFiles = await getFilesFromPath(newPath);
+      files = [...files, ...newFiles];
     }
   }
 
-  files.push(new File([buffer], "data.json"))
-  console.log("FINAL FILES ARRAY", files)
+  files.push(new File([buffer], "data.json"));
+  console.log("FINAL FILES ARRAY", files);
 
   return files;
 }
